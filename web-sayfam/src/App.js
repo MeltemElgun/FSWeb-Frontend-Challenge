e89c4530ac300cb7e73b7e6ddc8136aa6a120738
MeltemElgun <113603458+MeltemElgun@users.noreply.github.com>
2023-03-15 02:05:07 +0300

    jsonserver,axios,useState

--- web-sayfam/src/App.js
+++ web-sayfam/src/App.js
@@ -1,3 +1,6 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
 import Introduce from "./components/Introduce";
 import Skills from "./components/Skills";
 import Profile from "./components/Profile";
@@ -5,12 +8,27 @@ import Projects from "./components/Projects";
 import Footer from "./components/Footer";
 import "@fontsource/inter";
 function App() {
+  const [projects, setProjects] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/projects")
+      .then((res) => {
+        if (res.status === 200) {
+          setProjects(res.data);
+        }
+        console.log(res);
+      })
+      .catch((error) => console.log(error));
+  }, []);
   return (
     <div className="App">
       <Introduce />
       <Skills />
       <Profile />
-      <Projects />
+      {projects.map((project) => (
+        <Projects key={project.id} pr={project} />
+      ))}
+
       <Footer />
     </div>
   );
--- web-sayfam/src/components/Projects.js
+++ web-sayfam/src/components/Projects.js
@@ -1,47 +1,24 @@
 import React from "react";
 import project from "./Projects.module.css";
-export default function Projects() {
+export default function Projects(props) {
+  const { name, description, relative, img } = props.pr;
   return (
-    <div className={project.box}>
-      <div className={project.p}>Projects</div>
-      <div className={project.cards}>
-        <div className={project.cardA}>
-          <p className={project.head}>Random Jokes</p>
-          <p className={project.textA}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-            aut, odit laborum aliquam voluptatum nisi mollitia.
-          </p>
-          <div className={project.spanA}>
-            <span>react</span>
-            <span>vercel</span>
-            <span>axios</span>
-            <span>router</span>
-          </div>
-          <a className={project.a} href="#">
-            View on Github
-          </a>
-          <a href="#">Go to app</a>{" "}
-          <img className={project.imgA} src="resim1.png" />
+    <div className={project.cards}>
+      <div className={project.cardA}>
+        <p className={project.head}>{name}</p>
+        <p className={project.textA}>{description}</p>
+        <div className={project.spanA}>
+          <span>{relative[0]}</span>
+          <span>{relative[1]}</span>
+          <span>{relative[2]}</span>
+          <span>{relative[3]}</span>
+          <span>{relative[4]}</span>
         </div>
-        <div className={project.cardB}>
-          <p className={project.head}>Are you bored?</p>
-          <p className={project.textB}>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Veniam
-            aut, odit laborum aliquam voluptatum nisi mollitia minima accusamus
-            ratione soluta aperiam sit voluptate? Dicta quod deserunt quam
-            temporibus cumque magnam!
-          </p>
-          <div className={project.spanB}>
-            <span>react</span>
-            <span>redux</span>
-            <span>axios</span>
-            <span>router</span>
-            <span>vercel</span>
-          </div>
-          <a className={project.a}>View on Github</a>
-          <a>Go to app</a>
-          <img className={project.imgB} src="resim2.png" />
-        </div>
+        <a className={project.a} href="#">
+          View on Github
+        </a>
+        <a href="#">Go to app</a>{" "}
+        <img className={project.imgA} src="resim1.png" />
       </div>
     </div>
   );
